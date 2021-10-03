a9bf6edabbff446abcf13c39eeadfffcb5517c6a
Jake Colledge <jakecolledgework@gmail.com>
2021-10-03 18:48:27 +1100

    added a function to change list colours

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-
-
 var timer = setInterval(myTimer, 1000)
-var test = $('.time')
-console.log(test.value)
+
 function myTimer(){
     var currentTime = moment()
     $('#currentDay').text(currentTime.format('MMMM Do YYYY, h:mm:ss a'))
 }
+/*if(timePast.format('H') > 9){
+    pastTime.addClass('.list-group-item past')
+}*/
+
+$('button').each(function(index){
+    console.log($(this).attr('id'))
+    var currentTime = moment()
+    var timeAttr = $(this).attr('id')
+    if(timeAttr < currentTime.format('H')){
+        $(this).addClass('.list-group-item past')
+    }
+})
