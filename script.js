2b110f7ad24ef4d0cb09cc12986a37eb6f53d127
Jake Colledge <jakecolledgework@gmail.com>
2021-10-04 12:22:30 +1030

    channged hover styling on past objects

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ $('button').each(function(){
     var currentTime = moment()
     var timeAttr = parseInt($(this).attr('id'))
     if(timeAttr < parseInt(currentTime.format('H'))){
-        console.log(timeAttr + ' ' + currentTime.format('H'))
         $(this).addClass('.list-group-item past')
-    }else if(timeAttr === 11){
+        $(this).unbind('mouseenter mouseleave')
+    }else if(timeAttr === parseInt(currentTime.format('H'))){
         $(this).addClass('.list-group-item present')
     }else{
         $(this).addClass('.list-group-item future')
