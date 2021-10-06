9b40af6e5c0d84bc817d9ee88cb22013d87523b2
Jake Colledge <jakecolledgework@gmail.com>
2021-10-06 17:46:48 +0500

    added function to create list items on load

--- script.js
+++ script.js
@@ -17,13 +17,52 @@ $('li').each(function(){
         $(this).addClass('.list-group-item future')
     }
 })
+var localStoredNotes = []
+localStoredNotes = JSON.parse(localStorage.getItem("listArray"))
 var addTextBtn = $('.saveBtn')
 addTextBtn.click(addListItem)
+
+function getSavedNotes(){
+    localStoredNotes = [JSON.parse(localStorage.getItem(listItems))]
+    var eachList = $('.time-block').children()
+    var startingDiv = 9
+    
+    $('<div>', {
+        class: 'schedule-list',
+    }).appendTo('.note-area');
+    
+    $('<ul>', {
+        class: 'added-items',
+    }).appendTo('.schedule-list');
+    
+    for(i=0;i<eachList.length;i++){
+        for(y=0; y<localStoredNotes[i].length; y++){
+            var currentListTime = $('#' + (startingDiv+i)).find('.added-items')
+            var localStoredNotesTime = [localStoredNotes[i]]
+            if(!localStoredNotesTime[y].length === 0){
+                $('<li>').appendTo(currentListTime);
+                $('<div>', {
+                    class: "input-group-prepend",
+                }).appendTo(currentListTime.children('li'));
+                $('<div>', {
+                    class: "input-group-text",
+                }).appendTo(currentListTime.children('.input-group-prepend'));
+                $('<p>').appendTo(currentListTime.children('.input-group-text'));
+                $('<input>', {
+                    type: "checkox",
+                }).appendTo(currentListTime.children('.input-group-text'));
+                $(currentListTime.children('p')).text('test')
+            }
+        }
+    }
+}
 
 function addListItem(event){
     var buttonId = $(this).parent().attr('id')
     var textInput = $('#' + buttonId).find('input').val()
-    if(textInput){
-        
+    if(!textInput){
+        $('#' + buttonId).find('input').css('box-shadow', '0px 0px 5px 1px red')
+    }else{
+
     }
 }
