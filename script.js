2eb5ca46befebb0957490ed4a1c140cf6a1bede0
Jake Colledge <jakecolledgework@gmail.com>
2021-10-07 18:52:17 +0500

    added function to add list item when button is clicked

--- script.js
+++ script.js
@@ -22,8 +22,19 @@ var localStoredNotes = []
 var addTextBtn = $('.saveBtn')
 addTextBtn.click(addListItem)
 
+create2dArray()
+getSavedNotes()
+
+function create2dArray(){
+    for(i=0;i<9;i++){
+        localStoredNotes[i] = ['']
+    }
+}
+
 function getSavedNotes(){
-    localStoredNotes = JSON.parse(localStorage.getItem("listArray"))
+    if(localStoredNotes === null){
+        localStoredNotes = JSON.parse(localStorage.getItem("listArray"))
+    }
     console.log(localStoredNotes)
     var eachList = $('.time-block').children()
     var startingDiv = 9
@@ -35,27 +46,30 @@ function getSavedNotes(){
     $('<ul>', {
         class: 'added-items',
     }).appendTo('.schedule-list');
-    
-    for(i=0;i<eachList.length;i++){
-        var listItemsArr = localStoredNotes[i]
-        console.log(listItemsArr)
-        for(j=0; j<listItemsArr.length; j++){
-            var currentListTime = $('#' + (startingDiv+i)).find('.added-items')
-            var localStoredNotesTime = localStoredNotes[i]
-            if(localStoredNotesTime[j]){
-                /*$('<li>').appendTo(currentListTime);
-                $('<div>', {
-                    class: "input-group-prepend",
-                }).appendTo(currentListTime.children('li'));
-                $('<div>', {
-                    class: "input-group-text",
-                }).appendTo(currentListTime.children('.input-group-prepend'));
-                $(currentListTime).find('.input-group-prepend').append('<p></p>')
-                $(currentListTime).find('.input-group-prepend').append('<input type="checkbox">')
-                $('<input>', {
-                    type: "checkox",
-                }).appendTo(currentListTime.children('.input-group-text'));*/
-                $(currentListTime).append('<li><div class="input-group-prepend"><div class="input-group-text"><p>hello</p><input type="checkbox"></div></div></li>')
+    if(localStoredNotes.length === 0){
+        return
+    }else{
+        for(i=0;i<eachList.length;i++){
+            var listItemsArr = localStoredNotes[i]
+            console.log(listItemsArr)
+            for(j=0; j<listItemsArr.length; j++){
+                var currentListTime = $('#' + (startingDiv+i)).find('.added-items')
+                var localStoredNotesTime = localStoredNotes[i]
+                if(localStoredNotesTime[j]){
+                    /*$('<li>').appendTo(currentListTime);
+                    $('<div>', {
+                        class: "input-group-prepend",
+                    }).appendTo(currentListTime.children('li'));
+                    $('<div>', {
+                        class: "input-group-text",
+                    }).appendTo(currentListTime.children('.input-group-prepend'));
+                    $(currentListTime).find('.input-group-prepend').append('<p></p>')
+                    $(currentListTime).find('.input-group-prepend').append('<input type="checkbox">')
+                    $('<input>', {
+                        type: "checkox",
+                    }).appendTo(currentListTime.children('.input-group-text'));*/
+                    $(currentListTime).append('<li><div class="input-group-prepend"><div class="input-group-text"><p>hello</p><input type="checkbox"></div></div></li>')
+                }
             }
         }
     }
@@ -63,10 +77,13 @@ function getSavedNotes(){
 
 function addListItem(event){
     var buttonId = $(this).parent().attr('id')
-    var textInput = $('#' + buttonId).find('input').val()
-    if(!textInput){
-        $('#' + buttonId).find('input').css('box-shadow', '0px 0px 5px 1px red')
+    var textInput = $('#' + buttonId).find('input')
+    if(!textInput.val()){
+        textInput.css('box-shadow', '0px 0px 5px 1px red')
     }else{
-
+        $('#' + buttonId).find('.added-items').append('<li><div class="input-group-prepend"><div class="input-group-text"><p></p><input type="checkbox"></div></div></li>')
+        $('#' + buttonId).find('.added-items li').last().find('p').text(textInput.val())
+        textInput.css('box-shadow', 'none')
+        textInput.val('')
     }
 }
